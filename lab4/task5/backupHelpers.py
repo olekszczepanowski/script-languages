703e144c36c21234979408d0c86d41799e5bd45e
olekszczepanowski <olekszczepanowski04@wp.pl>
2024-04-15 21:58:13 +0200

    add: just some prompt changes

--- lab4/task5/backupHelpers.py
+++ lab4/task5/backupHelpers.py
@@ -4,11 +4,11 @@ import sys
 def pathVerifier():
     folderPath = None  
     if len(sys.argv) < 2:
-        print('No folder path provided, enclose path in quotes if it contains spaces.')
+        print('Brak podanego folderu.')
     else:
         provided_path = sys.argv[1]
         if not os.path.isdir(provided_path):
-            print(f'Folder not found: {provided_path}')
+            print(f'Brak folderu: {provided_path}')
         else:
             folderPath = provided_path
     return folderPath
